cfb5b1851b468e4528189d9a3438f1d1304fbd28
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-10 10:49:59 -0300

    test: ensure Signup Controller return 500 if AddAccount throws

--- src/presentation/controllers/account/signup/signup-controller.spec.ts
+++ src/presentation/controllers/account/signup/signup-controller.spec.ts
@@ -1,5 +1,12 @@
 import { SignupController } from './signup-controller'
-import { AccountModel, AddAccount, AddAccountModel, HttpRequest } from './signup-controller-protocols'
+import {
+  AccountModel,
+  AddAccount,
+  AddAccountModel,
+  HttpRequest,
+  ServerError,
+  serverError
+} from './signup-controller-protocols'
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
@@ -51,5 +58,15 @@ describe('Signup Controller', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+  })
+
+  test('Should return 500 if AddAccount throws', async () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(async () => {
+      // eslint-disable-next-line @typescript-eslint/return-await
+      return new Promise((resolve, reject) => reject(new Error()))
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
   })
 })
