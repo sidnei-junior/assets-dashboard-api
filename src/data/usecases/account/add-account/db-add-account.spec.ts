fe2df29dc0efa33d03e0d9c9bcb172ca85c7d67d
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-10 05:41:57 -0300

    test: ensure Hasher call with correct password

---
+++ src/data/usecases/account/add-account/db-add-account.spec.ts
@@ -0,0 +1,39 @@
+import { Hasher } from '@/data/protocols/criptography/hasher'
+import { AddAccountModel } from '@/domain/usecases/account/add-acount'
+import { DbAddAccount } from './db-add-account'
+
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash(value: string): Promise<string> {
+      // eslint-disable-next-line @typescript-eslint/return-await
+      return new Promise((resolve) => resolve('hashed_password'))
+    }
+  }
+  return new HasherStub()
+}
+
+const makeFakeAccountData = (): AddAccountModel => ({
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password'
+})
+
+type SutTypes = {
+  sut: DbAddAccount
+  hasherStub: Hasher
+}
+
+const makeSut = (): SutTypes => {
+  const hasherStub = makeHasher()
+  const sut = new DbAddAccount(hasherStub)
+  return { sut, hasherStub }
+}
+
+describe('DbAddAccount Usecase', () => {
+  test('Should call Hasher with correct password', async () => {
+    const { sut, hasherStub } = makeSut()
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+    await sut.add(makeFakeAccountData())
+    expect(hashSpy).toHaveBeenCalledWith('valid_password')
+  })
+})
