5ae2e8191dd704fc780f1b47ae19598eaa6a5820
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-12 10:53:18 -0300

    test: ensure CompanyMongoRepository return a company on add success

---
+++ src/infra/db/mongodb/company/company-mongo-repository.spec.ts
@@ -0,0 +1,40 @@
+import { Collection } from 'mongodb'
+
+import env from '@/main/config/env'
+import { MongoHelper } from '../helper/mongo-helper'
+import { CompanyMongoRepository } from './company-mongo-repository'
+
+let companyCollection: Collection
+
+describe('Company Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    companyCollection = await MongoHelper.getCollection('companies')
+    await companyCollection.deleteMany({})
+  })
+
+  const makeSut = (): CompanyMongoRepository => {
+    return new CompanyMongoRepository()
+  }
+
+  describe('add()', () => {
+    test('Should return a company on add success', async () => {
+      const sut = makeSut()
+      const company = await sut.add({
+        name: 'any_name',
+        cnpj: 'any_cnpj'
+      })
+      expect(company).toBeTruthy()
+      expect(company.id).toBeTruthy()
+      expect(company.name).toBe('any_name')
+      expect(company.cnpj).toBe('any_cnpj')
+    })
+  })
+})
