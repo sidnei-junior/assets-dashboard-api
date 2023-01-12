fabcf6750d78632d4054cdda6d555728a1488b55
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-12 20:55:21 -0300

    feat: ensure AddCompany Controller return 400 if Validation returns an error

--- src/presentation/controllers/company/add-company/add-company-controller.ts
+++ src/presentation/controllers/company/add-company/add-company-controller.ts
@@ -1,15 +1,24 @@
 import { AddCompany } from '@/domain/usecases/company/add-company'
 import { CnpjInUseError } from '@/presentation/errors/cnpj-in-use-error'
 import { Controller, HttpRequest, HttpResponse, Validation } from '../../account/login/login-controller-protocols'
-import { forbidden, noContent, ServerError, serverError } from '../../account/signup/signup-controller-protocols'
+import {
+  badRequest,
+  forbidden,
+  noContent,
+  ServerError,
+  serverError
+} from '../../account/signup/signup-controller-protocols'
 
 export class AddCompanyController implements Controller {
   constructor(private readonly addCompany: AddCompany, private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      this.validation.validate(httpRequest.body)
+      const error = this.validation.validate(httpRequest.body)
 
+      if (error) {
+        return badRequest(error)
+      }
       const { name, cnpj } = httpRequest.body
       const company = await this.addCompany.add({ name, cnpj })
 
