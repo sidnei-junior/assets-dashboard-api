84bed38ad0be4b738531a12d3409807b8f015ea8
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-12 08:49:25 -0300

    test: ensure DbAddCompany call AddCompanyRepository with correct values

---
+++ src/data/usecases/company/add-company/db-add-company.spec.ts
@@ -0,0 +1,48 @@
+import { AddCompanyRepository } from '@/data/protocols/db/company/add-company-repository'
+import { CompanyModel } from '@/domain/modals/company'
+import { AddCompanyModel } from '@/domain/usecases/company/add-company'
+import { DbAddCompany } from './db-add-company'
+
+const makeFakeCompany = (): CompanyModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  cnpj: 'valid_cnpj'
+})
+
+const makeFakeCompanyData = (): AddCompanyModel => ({
+  name: 'valid_name',
+  cnpj: 'valid_cnpj'
+})
+
+const makeAddCompanyRepository = (): AddCompanyRepository => {
+  class AddCompanyRepositoryStub implements AddCompanyRepository {
+    async add(CompanyData: AddCompanyModel): Promise<CompanyModel> {
+      // eslint-disable-next-line @typescript-eslint/return-await
+      return new Promise((resolve) => resolve(makeFakeCompany()))
+    }
+  }
+  return new AddCompanyRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbAddCompany
+  addCompanyRepositoryStub: AddCompanyRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addCompanyRepositoryStub = makeAddCompanyRepository()
+  const sut = new DbAddCompany(addCompanyRepositoryStub)
+  return { sut, addCompanyRepositoryStub }
+}
+
+describe('DbAddCompany Usecase', () => {
+  test('Should call AddCompanyRepository with correct values', async () => {
+    const { sut, addCompanyRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addCompanyRepositoryStub, 'add')
+    await sut.add(makeFakeCompanyData())
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      cnpj: 'valid_cnpj'
+    })
+  })
+})
