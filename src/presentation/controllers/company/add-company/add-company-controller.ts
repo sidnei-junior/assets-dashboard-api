dd9c4fff44a6fc457f1b42fd3a07c65e9d8025fc
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-12 20:53:14 -0300

    feat: ensure AddCompany Controller call Validation with correct values

--- src/presentation/controllers/company/add-company/add-company-controller.ts
+++ src/presentation/controllers/company/add-company/add-company-controller.ts
@@ -1,13 +1,15 @@
 import { AddCompany } from '@/domain/usecases/company/add-company'
 import { CnpjInUseError } from '@/presentation/errors/cnpj-in-use-error'
-import { Controller, HttpRequest, HttpResponse } from '../../account/login/login-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from '../../account/login/login-controller-protocols'
 import { forbidden, noContent, ServerError, serverError } from '../../account/signup/signup-controller-protocols'
 
 export class AddCompanyController implements Controller {
-  constructor(private readonly addCompany: AddCompany) {}
+  constructor(private readonly addCompany: AddCompany, private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
+
       const { name, cnpj } = httpRequest.body
       const company = await this.addCompany.add({ name, cnpj })
 
