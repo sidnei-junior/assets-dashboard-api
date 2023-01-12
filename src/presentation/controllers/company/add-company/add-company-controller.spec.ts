4b6e0c912869a751bc40c4fd95194cbc2591781e
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-12 19:14:04 -0300

    test: ensure AddCompany Controller return 500 if AddCompany throws

--- src/presentation/controllers/company/add-company/add-company-controller.spec.ts
+++ src/presentation/controllers/company/add-company/add-company-controller.spec.ts
@@ -1,6 +1,7 @@
 import { CompanyModel } from '@/domain/modals/company'
 import { AddCompany, AddCompanyModel } from '@/domain/usecases/company/add-company'
 import { HttpRequest } from '../../account/login/login-controller-protocols'
+import { ServerError, serverError } from '../../account/signup/signup-controller-protocols'
 import { AddCompanyController } from './add-company-controller'
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -46,5 +47,15 @@ describe('AddCompany Controller', () => {
       name: 'any_name',
       cnpj: 'any_cnpj'
     })
+  })
+
+  test('Should return 500 if AddCompany throws', async () => {
+    const { sut, addCompanyStub } = makeSut()
+    jest.spyOn(addCompanyStub, 'add').mockImplementationOnce(async () => {
+      // eslint-disable-next-line @typescript-eslint/return-await
+      return new Promise((resolve, reject) => reject(new Error()))
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
   })
 })
