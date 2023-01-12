4ae252dc2a80b311a5f1b586aec895917bb17565
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-12 19:12:25 -0300

    feat: ensure AddCompany Controller return 500 if AddCompany throws

--- src/presentation/controllers/company/add-company/add-company-controller.ts
+++ src/presentation/controllers/company/add-company/add-company-controller.ts
@@ -1,12 +1,17 @@
 import { AddCompany } from '@/domain/usecases/company/add-company'
 import { Controller, HttpRequest, HttpResponse } from '../../account/login/login-controller-protocols'
+import { ServerError, serverError } from '../../account/signup/signup-controller-protocols'
 
 export class AddCompanyController implements Controller {
   constructor(private readonly addCompany: AddCompany) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name, cnpj } = httpRequest.body
-    await this.addCompany.add({ name, cnpj })
-    return null
+    try {
+      const { name, cnpj } = httpRequest.body
+      await this.addCompany.add({ name, cnpj })
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
