290bbdb3cd626cd4af5706d4145890911efdfb9e
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 10:28:56 -0300

    test: ensure DbDeleteCompany call DeleteCompanyRepository with correct value

---
+++ src/data/usecases/company/delete-company/db-delete-company.spec.ts
@@ -0,0 +1,30 @@
+import { DeleteCompanyRepository } from '@/data/protocols/db/company/delete-company-repository'
+import { DbDeleteCompany } from './db-delete-company'
+
+const makeDeleteCompanyRepository = (): DeleteCompanyRepository => {
+  class DeleteCompanyRepositoryStub implements DeleteCompanyRepository {
+    async delete(id: string): Promise<void> {}
+  }
+
+  return new DeleteCompanyRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbDeleteCompany
+  deleteCompanyRepositoryStub: DeleteCompanyRepository
+}
+
+const makeSut = (): SutTypes => {
+  const deleteCompanyRepositoryStub = makeDeleteCompanyRepository()
+  const sut = new DbDeleteCompany(deleteCompanyRepositoryStub)
+  return { sut, deleteCompanyRepositoryStub }
+}
+
+describe('DbDeleteCompany Usecase', () => {
+  test('Should call DeleteCompanyRepository with correct value', async () => {
+    const { sut, deleteCompanyRepositoryStub } = makeSut()
+    const deleteSpy = jest.spyOn(deleteCompanyRepositoryStub, 'delete')
+    await sut.delete('any_id')
+    expect(deleteSpy).toHaveBeenCalledWith('any_id')
+  })
+})
