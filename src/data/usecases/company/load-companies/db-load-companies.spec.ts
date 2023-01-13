065bcc6e6d3f1087794d4cda8bdfb427f41ae0b4
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 06:56:40 -0300

    test: ensure DbLoadCompanies call LoadCompaniesRepository

---
+++ src/data/usecases/company/load-companies/db-load-companies.spec.ts
@@ -0,0 +1,46 @@
+import { LoadCompaniesRepository } from '@/data/protocols/db/company/load-companies-repository'
+import { CompanyModel } from '@/domain/models/company'
+import { DbLoadCompanies } from './db-load-companies'
+
+const makeFakeCompanies = (): CompanyModel[] => [
+  {
+    id: 'any_id',
+    name: 'any_name',
+    cnpj: 'any_cnpj'
+  },
+  {
+    id: 'other_id',
+    name: 'other_name',
+    cnpj: 'other_cnpj'
+  }
+]
+
+const makeLoadCompaniesRepository = (): LoadCompaniesRepository => {
+  class LoadCompaniesRepositoryStub implements LoadCompaniesRepository {
+    async loadAll(): Promise<CompanyModel[]> {
+      return makeFakeCompanies()
+    }
+  }
+
+  return new LoadCompaniesRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadCompanies
+  loadCompaniesRepositoryStub: LoadCompaniesRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadCompaniesRepositoryStub = makeLoadCompaniesRepository()
+  const sut = new DbLoadCompanies(loadCompaniesRepositoryStub)
+  return { sut, loadCompaniesRepositoryStub }
+}
+
+describe('DbLoadCompanies Usecase', () => {
+  test('Should call LoadCompaniesRepository', async () => {
+    const { sut, loadCompaniesRepositoryStub } = makeSut()
+    const loadAllSpy = jest.spyOn(loadCompaniesRepositoryStub, 'loadAll')
+    await sut.load()
+    expect(loadAllSpy).toHaveBeenCalled()
+  })
+})
