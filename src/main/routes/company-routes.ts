e88b1f66b41d14d6e07fdfbb08879c357e7fadad
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 16:22:23 -0300

    feat: ensure return 403 on delete company without accessToken

--- src/main/routes/company-routes.ts
+++ src/main/routes/company-routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddCompanyController } from '../factories/controllers/company/add-company/add-company-controller-factory'
+import { makeDeleteCompanyController } from '../factories/controllers/company/delete-company/delete-company-controller-factory'
 import { makeLoadCompaniesController } from '../factories/controllers/company/load-companies/load-companies-controller-factory'
 import { adminAuth } from '../middlewares/admin-auth'
 import { auth } from '../middlewares/auth'
@@ -8,4 +9,5 @@ import { auth } from '../middlewares/auth'
 export default (router: Router): void => {
   router.post('/companies', adminAuth, adaptRoute(makeAddCompanyController()))
   router.get('/companies', auth, adaptRoute(makeLoadCompaniesController()))
+  router.delete('/companies/:companyId', auth, adaptRoute(makeDeleteCompanyController()))
 }
