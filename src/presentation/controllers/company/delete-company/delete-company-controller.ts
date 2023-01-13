86a4ee870e682cd8e7f4656ae6949f9bd2a63eb5
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 13:14:12 -0300

    feat: ensure DeleteCompany Controller return 500 if DeleteCompany throws

--- src/presentation/controllers/company/delete-company/delete-company-controller.ts
+++ src/presentation/controllers/company/delete-company/delete-company-controller.ts
@@ -1,13 +1,17 @@
 import { DeleteCompany } from '@/domain/usecases/company/delete-company'
-import { noContent, notFound } from '@/presentation/helpers/http/http-helper'
+import { noContent, notFound, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class DeleteCompanyController implements Controller {
   constructor(private readonly deleteCompany: DeleteCompany) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { companyId: id } = httpRequest.params
-    const response = await this.deleteCompany.delete(id)
-    return response === null ? notFound() : noContent()
+    try {
+      const { companyId: id } = httpRequest.params
+      const response = await this.deleteCompany.delete(id)
+      return response === null ? notFound() : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
