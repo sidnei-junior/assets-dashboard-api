da66bf3e5a519bf04d08e03be5cb25c5160ce58a
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 08:05:11 -0300

    feat: ensure LoadCompanies Controller return 200 on success

--- src/presentation/controllers/company/load-companies/load-companies-controller.ts
+++ src/presentation/controllers/company/load-companies/load-companies-controller.ts
@@ -1,11 +1,12 @@
 import { LoadCompanies } from '@/domain/usecases/company/load-companies'
+import { ok } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class LoadCompaniesController implements Controller {
   constructor(private readonly loadCompanies: LoadCompanies) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.loadCompanies.load()
-    return null
+    const companies = await this.loadCompanies.load()
+    return ok(companies)
   }
 }
