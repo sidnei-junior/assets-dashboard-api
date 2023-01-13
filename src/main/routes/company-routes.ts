c78b20e652b13b38d3e3f3b7e8774fc2faa5d3d8
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 09:16:33 -0300

    feat: ensure GET /companies return 403 on load companies without accessToken

--- src/main/routes/company-routes.ts
+++ src/main/routes/company-routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddCompanyController } from '../factories/controllers/company/add-company/add-company-controller-factory'
+import { makeLoadCompaniesController } from '../factories/controllers/company/load-companies/load-companies-controller-factory'
 import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
   router.post('/companies', adminAuth, adaptRoute(makeAddCompanyController()))
+  router.get('/companies', auth, adaptRoute(makeLoadCompaniesController()))
 }
