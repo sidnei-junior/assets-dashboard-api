9fbfe468d88ce79e7706b3d386d765920888ab14
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 08:35:07 -0300

    feat: ensure LoadCompanies Controller return 500 if LoadCompanies throws

--- src/presentation/controllers/company/load-companies/load-companies-controller.ts
+++ src/presentation/controllers/company/load-companies/load-companies-controller.ts
@@ -1,12 +1,16 @@
 import { LoadCompanies } from '@/domain/usecases/company/load-companies'
-import { noContent, ok } from '@/presentation/helpers/http/http-helper'
+import { noContent, ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class LoadCompaniesController implements Controller {
   constructor(private readonly loadCompanies: LoadCompanies) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const companies = await this.loadCompanies.load()
-    return companies.length ? ok(companies) : noContent()
+    try {
+      const companies = await this.loadCompanies.load()
+      return companies.length ? ok(companies) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
