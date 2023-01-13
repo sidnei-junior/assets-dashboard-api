9116cd3258d809f24d9bbbcbeed0339ab221f7c9
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 13:09:58 -0300

    feat: ensure DeleteCompany Controller return 204 on success

--- src/presentation/controllers/company/delete-company/delete-company-controller.ts
+++ src/presentation/controllers/company/delete-company/delete-company-controller.ts
@@ -1,5 +1,5 @@
 import { DeleteCompany } from '@/domain/usecases/company/delete-company'
-import { notFound } from '@/presentation/helpers/http/http-helper'
+import { noContent, notFound } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class DeleteCompanyController implements Controller {
@@ -8,9 +8,6 @@ export class DeleteCompanyController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { companyId: id } = httpRequest.params
     const response = await this.deleteCompany.delete(id)
-    if (response === null) {
-      return notFound()
-    }
-    return null
+    return response === null ? notFound() : noContent()
   }
 }
