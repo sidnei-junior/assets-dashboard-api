9b820a16c725759bf5c94b9dacd104ee160fba8c
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 11:37:49 -0300

    feat: create LoadUnitsByCompany Infra layer

--- src/infra/db/mongodb/unit/unit-mongo-repository.ts
+++ src/infra/db/mongodb/unit/unit-mongo-repository.ts
@@ -1,9 +1,10 @@
 import { AddUnitRepository } from '@/data/protocols/db/unit/add-unit-repository'
+import { LoadUnitsByCompanyRepository } from '@/data/protocols/db/unit/load-units-by-company-repository'
 import { UnitModel } from '@/domain/models/unit'
 import { AddUnitModel } from '@/domain/usecases/unit/add-unit'
 import { MongoHelper } from '../helper/mongo-helper'
 
-export class UnitMongoRepository implements AddUnitRepository {
+export class UnitMongoRepository implements AddUnitRepository, LoadUnitsByCompanyRepository {
   async add(unitData: AddUnitModel): Promise<UnitModel> {
     const unitCollection = await MongoHelper.getCollection('units')
     const result = await unitCollection.insertOne(unitData)
@@ -11,5 +12,11 @@ export class UnitMongoRepository implements AddUnitRepository {
     const unitById = await unitCollection.findOne({ _id: id })
     const unit = MongoHelper.map(unitById)
     return unit
+  }
+
+  async loadByCompanyId(companyId: string): Promise<UnitModel[]> {
+    const unitCollection = await MongoHelper.getCollection('units')
+    const units: UnitModel[] = (await unitCollection.find({ companyId }).toArray()) as unknown as UnitModel[]
+    return units.map((unit) => MongoHelper.map(unit))
   }
 }
