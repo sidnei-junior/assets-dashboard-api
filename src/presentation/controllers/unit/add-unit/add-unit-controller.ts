9165002db1bf18c243e2cdf2e86e0a25236d2863
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 09:08:56 -0300

    feat: ensure AddUnit Controller call Validation with correct values

--- src/presentation/controllers/unit/add-unit/add-unit-controller.ts
+++ src/presentation/controllers/unit/add-unit/add-unit-controller.ts
@@ -1,12 +1,14 @@
 import { AddUnit } from '@/domain/usecases/unit/add-unit'
-import { Controller, HttpRequest, HttpResponse } from '../../account/login/login-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from '../../account/login/login-controller-protocols'
 import { notFound, NotFoundError, ok, serverError } from '../../account/signup/signup-controller-protocols'
 
 export class AddUnitController implements Controller {
-  constructor(private readonly addUnit: AddUnit) {}
+  constructor(private readonly addUnit: AddUnit, private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      await this.validation.validate(httpRequest.body)
+
       const unit = await this.addUnit.add(httpRequest.body)
       if (unit === null) {
         return notFound(new NotFoundError('company'))
