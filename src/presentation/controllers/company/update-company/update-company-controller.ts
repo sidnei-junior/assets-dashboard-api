9ca77a8cb4d4299666ba8e967b946fe7dcdad88b
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 00:23:19 -0300

    feat: ensure UpdateCompany Controller return 404 if UpdateCompany return null

--- src/presentation/controllers/company/update-company/update-company-controller.ts
+++ src/presentation/controllers/company/update-company/update-company-controller.ts
@@ -1,5 +1,5 @@
 import { UpdateCompany } from '@/domain/usecases/company/update-company'
-import { serverError } from '@/presentation/helpers/http/http-helper'
+import { notFound, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class UpdateCompanyController implements Controller {
@@ -8,7 +8,10 @@ export class UpdateCompanyController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { companyId: id } = httpRequest.params
-      await this.updateCompany.update(httpRequest.body, id)
+      const company = await this.updateCompany.update(httpRequest.body, id)
+      if (company === null) {
+        return notFound()
+      }
       return null
     } catch (error) {
       return serverError(error)
