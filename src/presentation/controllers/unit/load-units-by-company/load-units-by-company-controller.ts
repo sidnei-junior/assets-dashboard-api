18fd7f596a61c11cb5e9e4bec18a3a333086c290
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 13:06:29 -0300

    feat: add Validation in Presentation layer

--- src/presentation/controllers/unit/load-units-by-company/load-units-by-company-controller.ts
+++ src/presentation/controllers/unit/load-units-by-company/load-units-by-company-controller.ts
@@ -1,12 +1,18 @@
 import { LoadUnitsByCompany } from '@/domain/usecases/unit/load-units-by-company'
-import { Controller, HttpRequest, HttpResponse } from '../../account/login/login-controller-protocols'
-import { noContent, notFound, NotFoundError, ok, serverError } from '../../account/signup/signup-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from '../../account/login/login-controller-protocols'
+import { badRequest, noContent, ok, serverError } from '../../account/signup/signup-controller-protocols'
 
 export class LoadUnitsByCompanyController implements Controller {
-  constructor(private readonly loadUnitsByCompany: LoadUnitsByCompany) {}
+  constructor(private readonly loadUnitsByCompany: LoadUnitsByCompany, private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+
+      if (error) {
+        return badRequest(error)
+      }
+
       const units = await this.loadUnitsByCompany.load(httpRequest.body.companyId)
       return units.length ? ok(units) : noContent()
     } catch (error) {
