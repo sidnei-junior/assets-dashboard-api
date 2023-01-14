8e036e847c4c15abe5296900bd89a95204f814c5
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 12:21:26 -0300

    feat: create LoadUnitsByCompany Presentation layer

---
+++ src/presentation/controllers/unit/load-units-by-company/load-units-by-company-controller.ts
@@ -0,0 +1,21 @@
+import { LoadUnitsByCompany } from '@/domain/usecases/unit/load-units-by-company'
+import { Controller, HttpRequest, HttpResponse } from '../../account/login/login-controller-protocols'
+import { noContent, notFound, NotFoundError, ok, serverError } from '../../account/signup/signup-controller-protocols'
+
+export class LoadUnitsByCompanyController implements Controller {
+  constructor(private readonly loadUnitsByCompany: LoadUnitsByCompany) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const units = await this.loadUnitsByCompany.load(httpRequest.body.companyId)
+
+      if (units === null) {
+        return notFound(new NotFoundError('company'))
+      }
+
+      return units.length ? ok(units) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
