ecc611a766238e039b29d09e969823aca3ff2634
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 08:43:26 -0300

    feat: ensure AddUnit Controller return 500 if AddUnit throws

--- src/presentation/controllers/unit/add-unit/add-unit-controller.ts
+++ src/presentation/controllers/unit/add-unit/add-unit-controller.ts
@@ -1,11 +1,16 @@
 import { AddUnit } from '@/domain/usecases/unit/add-unit'
 import { Controller, HttpRequest, HttpResponse } from '../../account/login/login-controller-protocols'
+import { serverError } from '../../account/signup/signup-controller-protocols'
 
 export class AddUnitController implements Controller {
   constructor(private readonly addUnit: AddUnit) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.addUnit.add(httpRequest.body)
-    return null
+    try {
+      await this.addUnit.add(httpRequest.body)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
