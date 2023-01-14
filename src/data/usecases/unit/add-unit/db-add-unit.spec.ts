e5a992eb584d02bd25aa612cf01bbbd22a04265b
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 07:27:26 -0300

    test: ensure DbAddUnit call LoadCompanyByIdRespository with correct companyId

--- src/data/usecases/unit/add-unit/db-add-unit.spec.ts
+++ src/data/usecases/unit/add-unit/db-add-unit.spec.ts
@@ -1,4 +1,6 @@
+import { LoadCompanyByIdRepository } from '@/data/protocols/db/company/load-company-by-id-repository'
 import { AddUnitRepository } from '@/data/protocols/db/unit/add-unit-repository'
+import { CompanyModel } from '@/domain/models/company'
 import { UnitModel } from '@/domain/models/unit'
 import { AddUnitModel } from '@/domain/usecases/unit/add-unit'
 import { DbAddUnit } from './db-add-unit'
@@ -14,6 +16,12 @@ const makeFakeUnitData = (): AddUnitModel => ({
   companyId: 'valid_company_id'
 })
 
+const makeFakeCompany = (): CompanyModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  cnpj: 'valid_cnpj'
+})
+
 const makeAddUnitRepository = (): AddUnitRepository => {
   class AddUnitRepositoryStub implements AddUnitRepository {
     async add(UnitData: AddUnitModel): Promise<UnitModel> {
@@ -23,15 +31,27 @@ const makeAddUnitRepository = (): AddUnitRepository => {
   return new AddUnitRepositoryStub()
 }
 
+const makeLoadCompanyByIdRepository = (): LoadCompanyByIdRepository => {
+  class LoadCompanyByIdRepositoryStub implements LoadCompanyByIdRepository {
+    async loadById(id: string): Promise<CompanyModel> {
+      return makeFakeCompany()
+    }
+  }
+
+  return new LoadCompanyByIdRepositoryStub()
+}
+
 type SutTypes = {
   sut: DbAddUnit
   addUnitRepositoryStub: AddUnitRepository
+  loadCompanyByIdRepositoryStub: LoadCompanyByIdRepository
 }
 
 const makeSut = (): SutTypes => {
   const addUnitRepositoryStub = makeAddUnitRepository()
-  const sut = new DbAddUnit(addUnitRepositoryStub)
-  return { sut, addUnitRepositoryStub }
+  const loadCompanyByIdRepositoryStub = makeLoadCompanyByIdRepository()
+  const sut = new DbAddUnit(addUnitRepositoryStub, loadCompanyByIdRepositoryStub)
+  return { sut, addUnitRepositoryStub, loadCompanyByIdRepositoryStub }
 }
 
 describe('DbAddUnit Usecase', () => {
@@ -49,5 +69,12 @@ describe('DbAddUnit Usecase', () => {
     const { sut } = makeSut()
     const unit = await sut.add(makeFakeUnitData())
     expect(unit).toEqual(makeFakeUnit())
+  })
+
+  test('Should call LoadCompanyByIdRespository with correct companyId', async () => {
+    const { sut, loadCompanyByIdRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadCompanyByIdRepositoryStub, 'loadById')
+    await sut.add(makeFakeUnitData())
+    expect(loadSpy).toHaveBeenCalledWith('valid_company_id')
   })
 })
