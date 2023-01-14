d007c3df2774047b4561eb741002a452783c21a3
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 15:56:22 -0300

    test: create UpdateUnit Data layer

---
+++ src/data/usecases/unit/update-unit/db-update-unit.spec.ts
@@ -0,0 +1,56 @@
+import { UpdateUnitRepository } from '@/data/protocols/db/unit/update-unit-repository'
+import { UnitModel } from '@/domain/models/unit'
+import { ResourceLabelNotFound, UpdateUnitModel } from '@/domain/usecases/unit/update-unit'
+import { DbUpdateUnit } from './db-update-unit'
+
+const makeUpdateUnitRepository = (): UpdateUnitRepository => {
+  class UpdateUnitRepositoryStub implements UpdateUnitRepository {
+    async update(unitData: UpdateUnitModel, id: string): Promise<UnitModel | ResourceLabelNotFound> {
+      return makeFakeUnit()
+    }
+  }
+  return new UpdateUnitRepositoryStub()
+}
+
+const makeFakeUnit = (): UnitModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  companyId: 'valid_company_id'
+})
+
+const makeFakeUnitData = (): UpdateUnitModel => ({
+  name: 'valid_name',
+  companyId: 'valid_company_id'
+})
+
+type SutTypes = {
+  sut: DbUpdateUnit
+  updateUnitRepositoryStub: UpdateUnitRepository
+}
+
+const makeSut = (): SutTypes => {
+  const updateUnitRepositoryStub = makeUpdateUnitRepository()
+  const sut = new DbUpdateUnit(updateUnitRepositoryStub)
+  return { sut, updateUnitRepositoryStub }
+}
+
+describe('DbUpdateUnit', () => {
+  test('Should call UpdateUnitRepository with correct values', async () => {
+    const { sut, updateUnitRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(updateUnitRepositoryStub, 'update')
+    await sut.update(makeFakeUnitData(), 'any_id')
+    expect(updateSpy).toHaveBeenCalledWith(
+      {
+        name: 'valid_name',
+        companyId: 'valid_company_id'
+      },
+      'any_id'
+    )
+  })
+
+  test('Should return a unit on success', async () => {
+    const { sut } = makeSut()
+    const unit = await sut.update(makeFakeUnitData(), 'any_id')
+    expect(unit).toEqual(makeFakeUnit())
+  })
+})
