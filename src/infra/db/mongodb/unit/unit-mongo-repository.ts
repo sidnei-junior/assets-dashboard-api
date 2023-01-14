1c52c527722149a562212a670fcc78923c8531df
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 14:48:26 -0300

    feat: create DeleteUnity Infra layer

--- src/infra/db/mongodb/unit/unit-mongo-repository.ts
+++ src/infra/db/mongodb/unit/unit-mongo-repository.ts
@@ -1,10 +1,12 @@
 import { AddUnitRepository } from '@/data/protocols/db/unit/add-unit-repository'
+import { DeleteUnitRepository } from '@/data/protocols/db/unit/delete-unit-repository'
 import { LoadUnitsByCompanyRepository } from '@/data/protocols/db/unit/load-units-by-company-repository'
 import { UnitModel } from '@/domain/models/unit'
 import { AddUnitModel } from '@/domain/usecases/unit/add-unit'
+import { ObjectId } from 'mongodb'
 import { MongoHelper } from '../helper/mongo-helper'
 
-export class UnitMongoRepository implements AddUnitRepository, LoadUnitsByCompanyRepository {
+export class UnitMongoRepository implements AddUnitRepository, LoadUnitsByCompanyRepository, DeleteUnitRepository {
   async add(unitData: AddUnitModel): Promise<UnitModel> {
     const unitCollection = await MongoHelper.getCollection('units')
     const result = await unitCollection.insertOne(unitData)
@@ -18,5 +20,15 @@ export class UnitMongoRepository implements AddUnitRepository, LoadUnitsByCompan
     const unitCollection = await MongoHelper.getCollection('units')
     const units: UnitModel[] = (await unitCollection.find({ companyId }).toArray()) as unknown as UnitModel[]
     return units.map((unit) => MongoHelper.map(unit))
+  }
+
+  async delete(id: string): Promise<void> {
+    const mongoId = new ObjectId(id)
+    const unitCollection = await MongoHelper.getCollection('units')
+    const mongoResponse = await unitCollection.deleteOne({ _id: mongoId })
+    const { deletedCount } = mongoResponse
+    if (deletedCount === 0) {
+      return null
+    }
   }
 }
