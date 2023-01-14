034f503bd78c7af399532fc5b344c162d4abcce4
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 00:18:38 -0300

    feat: ensure UpdateCompany Controller return 500 if UpdateCompany throw

--- src/presentation/controllers/company/update-company/update-company-controller.ts
+++ src/presentation/controllers/company/update-company/update-company-controller.ts
@@ -1,12 +1,17 @@
 import { UpdateCompany } from '@/domain/usecases/company/update-company'
+import { serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class UpdateCompanyController implements Controller {
   constructor(private readonly updateCompany: UpdateCompany) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { companyId: id } = httpRequest.params
-    await this.updateCompany.update(httpRequest.body, id)
-    return null
+    try {
+      const { companyId: id } = httpRequest.params
+      await this.updateCompany.update(httpRequest.body, id)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
