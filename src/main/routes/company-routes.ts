f6148dc43e454b6e2da90e2f57b3c47ef35cced9
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 05:51:48 -0300

    feat: ensure UpdateCompany route return 403 on update without accessToken

--- src/main/routes/company-routes.ts
+++ src/main/routes/company-routes.ts
@@ -3,6 +3,7 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddCompanyController } from '../factories/controllers/company/add-company/add-company-controller-factory'
 import { makeDeleteCompanyController } from '../factories/controllers/company/delete-company/delete-company-controller-factory'
 import { makeLoadCompaniesController } from '../factories/controllers/company/load-companies/load-companies-controller-factory'
+import { makeUpdateCompanyController } from '../factories/controllers/company/update-company/update-company-controller-factory'
 import { adminAuth } from '../middlewares/admin-auth'
 import { auth } from '../middlewares/auth'
 
@@ -10,4 +11,5 @@ export default (router: Router): void => {
   router.post('/companies', adminAuth, adaptRoute(makeAddCompanyController()))
   router.get('/companies', auth, adaptRoute(makeLoadCompaniesController()))
   router.delete('/companies/:companyId', auth, adaptRoute(makeDeleteCompanyController()))
+  router.put('/companies/:companyId', auth, adaptRoute(makeUpdateCompanyController()))
 }
