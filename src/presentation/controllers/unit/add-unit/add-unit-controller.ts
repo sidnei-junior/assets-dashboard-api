218f102ee1691841846981042e3a56065fc865f6
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 09:13:32 -0300

    feat: ensure AddUnit Controller return 400 if Validation returns an error

--- src/presentation/controllers/unit/add-unit/add-unit-controller.ts
+++ src/presentation/controllers/unit/add-unit/add-unit-controller.ts
@@ -1,18 +1,24 @@
 import { AddUnit } from '@/domain/usecases/unit/add-unit'
 import { Controller, HttpRequest, HttpResponse, Validation } from '../../account/login/login-controller-protocols'
-import { notFound, NotFoundError, ok, serverError } from '../../account/signup/signup-controller-protocols'
+import { badRequest, notFound, NotFoundError, ok, serverError } from '../../account/signup/signup-controller-protocols'
 
 export class AddUnitController implements Controller {
   constructor(private readonly addUnit: AddUnit, private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.validation.validate(httpRequest.body)
+      const error = this.validation.validate(httpRequest.body)
+
+      if (error) {
+        return badRequest(error)
+      }
 
       const unit = await this.addUnit.add(httpRequest.body)
+
       if (unit === null) {
         return notFound(new NotFoundError('company'))
       }
+
       return ok(unit)
     } catch (error) {
       return serverError(error)
