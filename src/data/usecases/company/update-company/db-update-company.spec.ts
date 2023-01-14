ca2a39daa22562db885020a4cffa60e677a45a72
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-13 22:24:51 -0300

    test: ensure DbUpdateCompany call UpdateCompanyRepository with correct values

---
+++ src/data/usecases/company/update-company/db-update-company.spec.ts
@@ -0,0 +1,47 @@
+import { UpdateCompanyRepository } from '@/data/protocols/db/company/update-company-repository'
+import { CompanyModel } from '@/domain/models/company'
+import { UpdateCompanyModel } from '@/domain/usecases/company/update-company'
+import { DbUpdateCompany } from './db-update-company'
+
+const makeUpdateCompanyRepository = (): UpdateCompanyRepository => {
+  class UpdateCompanyRepositoryStub implements UpdateCompanyRepository {
+    async update(companyData: UpdateCompanyModel): Promise<CompanyModel> {
+      return makeFakeCompany()
+    }
+  }
+  return new UpdateCompanyRepositoryStub()
+}
+
+const makeFakeCompany = (): CompanyModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  cnpj: 'valid_cnpj'
+})
+
+const makeFakeCompanyData = (): UpdateCompanyModel => ({
+  name: 'valid_name',
+  cnpj: 'valid_cnpj'
+})
+
+type SutTypes = {
+  sut: DbUpdateCompany
+  updateCompanyRepositoryStub: UpdateCompanyRepository
+}
+
+const makeSut = (): SutTypes => {
+  const updateCompanyRepositoryStub = makeUpdateCompanyRepository()
+  const sut = new DbUpdateCompany(updateCompanyRepositoryStub)
+  return { sut, updateCompanyRepositoryStub }
+}
+
+describe('DbUpdateCompany', () => {
+  test('Should call UpdateCompanyRepository with correct values', async () => {
+    const { sut, updateCompanyRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(updateCompanyRepositoryStub, 'update')
+    await sut.update(makeFakeCompanyData())
+    expect(updateSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      cnpj: 'valid_cnpj'
+    })
+  })
+})
