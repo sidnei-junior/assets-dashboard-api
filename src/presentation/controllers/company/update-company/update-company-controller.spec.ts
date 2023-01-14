dda30a89a2acfc3c270c57c61058dfa9f432c45e
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 00:38:28 -0300

    test: ensure UpdateCompany Controller return 400 if Validation returns an error

--- src/presentation/controllers/company/update-company/update-company-controller.spec.ts
+++ src/presentation/controllers/company/update-company/update-company-controller.spec.ts
@@ -1,7 +1,7 @@
 import { CompanyModel } from '@/domain/models/company'
 import { UpdateCompany, UpdateCompanyModel } from '@/domain/usecases/company/update-company'
-import { ServerError } from '@/presentation/errors'
-import { notFound, ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { MissingParamError, ServerError } from '@/presentation/errors'
+import { badRequest, notFound, ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { HttpRequest, Validation } from '@/presentation/protocols'
 import { UpdateCompanyController } from './update-company-controller'
 
@@ -93,5 +93,12 @@ describe('UpdateCompany Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
