41fd692305f42a317f09afde02290a3c5f57b62e
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 17:18:14 -0300

    feat: create UpdateUnit Infra layer

--- src/infra/db/mongodb/unit/unit-mongo-repository.ts
+++ src/infra/db/mongodb/unit/unit-mongo-repository.ts
@@ -1,12 +1,16 @@
 import { AddUnitRepository } from '@/data/protocols/db/unit/add-unit-repository'
 import { DeleteUnitRepository } from '@/data/protocols/db/unit/delete-unit-repository'
 import { LoadUnitsByCompanyRepository } from '@/data/protocols/db/unit/load-units-by-company-repository'
+import { UpdateUnitRepository } from '@/data/protocols/db/unit/update-unit-repository'
 import { UnitModel } from '@/domain/models/unit'
 import { AddUnitModel } from '@/domain/usecases/unit/add-unit'
+import { UpdateUnitModel } from '@/domain/usecases/unit/update-unit'
 import { ObjectId } from 'mongodb'
 import { MongoHelper } from '../helper/mongo-helper'
 
-export class UnitMongoRepository implements AddUnitRepository, LoadUnitsByCompanyRepository, DeleteUnitRepository {
+export class UnitMongoRepository
+  implements AddUnitRepository, LoadUnitsByCompanyRepository, DeleteUnitRepository, UpdateUnitRepository
+{
   async add(unitData: AddUnitModel): Promise<UnitModel> {
     const unitCollection = await MongoHelper.getCollection('units')
     const result = await unitCollection.insertOne(unitData)
@@ -30,5 +34,17 @@ export class UnitMongoRepository implements AddUnitRepository, LoadUnitsByCompan
     if (deletedCount === 0) {
       return null
     }
+  }
+
+  async update(unitData: UpdateUnitModel, id: string): Promise<UnitModel | string> {
+    const mongoId = new ObjectId(id)
+    const unitCollection = await MongoHelper.getCollection('units')
+    const { matchedCount } = await unitCollection.updateOne({ _id: mongoId }, { $set: { ...unitData } })
+    if (matchedCount === 0) {
+      return 'unit'
+    }
+    const unitById = await unitCollection.findOne({ _id: mongoId })
+    const unit = MongoHelper.map(unitById)
+    return unit
   }
 }
