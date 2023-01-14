bb82b6004a4f1bffcb8c7d614de855554ced0608
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-14 17:28:39 -0300

    feat: add LoadCompanyByIdRepository

--- src/data/usecases/unit/update-unit/db-update-unit.ts
+++ src/data/usecases/unit/update-unit/db-update-unit.ts
@@ -1,11 +1,21 @@
+import { LoadCompanyByIdRepository } from '@/data/protocols/db/company/load-company-by-id-repository'
 import { UpdateUnitRepository } from '@/data/protocols/db/unit/update-unit-repository'
 import { UnitModel } from '@/domain/models/unit'
 import { UpdateUnit, UpdateUnitModel } from '@/domain/usecases/unit/update-unit'
 
 export class DbUpdateUnit implements UpdateUnit {
-  constructor(private readonly updateUnitRepository: UpdateUnitRepository) {}
+  constructor(
+    private readonly updateUnitRepository: UpdateUnitRepository,
+    private readonly loadCompanyByIdRepository: LoadCompanyByIdRepository
+  ) {}
 
   async update(unitData: UpdateUnitModel, id: string): Promise<UnitModel | string> {
+    const response = await this.loadCompanyByIdRepository.loadById(unitData.companyId)
+
+    if (response === null) {
+      return 'company'
+    }
+
     const unit = await this.updateUnitRepository.update(unitData, id)
     return unit
   }
