a4e9de43e3db322cf364c08ad996c2cb426cf3a8
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-15 16:57:37 -0300

    fix: folder name

--- src/main/factories/controllers/unit/update-unit/update-unit-controller-factory.ts
+++ src/main/factories/controllers/unit/update-unit/update-unit-controller-factory.ts
@@ -2,7 +2,7 @@ import { makeLogControllerDecorator } from '@/main/factories/decorators/log-cont
 import { makeDbUpdateUnit } from '@/main/factories/usecases/unit/update-unit/db-update-unit-factory'
 import { UpdateUnitController } from '@/presentation/controllers/unit/update-unit/update-unit-controller'
 import { Controller } from '@/presentation/protocols'
-import { makeAddUnitValidation } from '../unit-company/add-unit-validation-factory'
+import { makeAddUnitValidation } from '../add-unit/add-unit-validation-factory'
 
 export const makeUpdateUnitController = (): Controller => {
   const controller = new UpdateUnitController(makeDbUpdateUnit(), makeAddUnitValidation())
