00c5788feada372fb6300d8a0d6bdcbc647b5034
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-15 20:53:39 -0300

    feat: create LoadAssetsByUnit Infra layer

--- src/infra/db/mongodb/asset/asset-mongo-repository.ts
+++ src/infra/db/mongodb/asset/asset-mongo-repository.ts
@@ -1,9 +1,10 @@
 import { AddAssetRepository } from '@/data/protocols/db/asset/add-asset-repository'
+import { LoadAssetsByUnitRepository } from '@/data/protocols/db/asset/load-assets-by-unit-repository'
 import { AssetModel } from '@/domain/models/asset'
 import { AddAssetModel } from '@/domain/usecases/asset/add-asset'
 import { MongoHelper } from '../helper/mongo-helper'
 
-export class AssetMongoRepository implements AddAssetRepository {
+export class AssetMongoRepository implements AddAssetRepository, LoadAssetsByUnitRepository {
   async add(assetData: AddAssetModel): Promise<AssetModel> {
     const assetCollection = await MongoHelper.getCollection('assets')
     const result = await assetCollection.insertOne(assetData)
@@ -11,5 +12,11 @@ export class AssetMongoRepository implements AddAssetRepository {
     const assetById = await assetCollection.findOne({ _id: id })
     const asset = MongoHelper.map(assetById)
     return asset
+  }
+
+  async loadByUnitId(unitId: string): Promise<AssetModel[]> {
+    const assetCollection = await MongoHelper.getCollection('assets')
+    const assets: AssetModel[] = (await assetCollection.find({ unitId }).toArray()) as unknown as AssetModel[]
+    return assets.map((asset) => MongoHelper.map(asset))
   }
 }
