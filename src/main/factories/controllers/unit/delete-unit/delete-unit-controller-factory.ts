6b937254c5717a4e286fca594385d20fb6a1c7c2
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-15 23:13:46 -0300

    fix: file name

--- src/main/factories/controllers/unit/delete-unit/delete-unit-controller-factory.ts
+++ src/main/factories/controllers/unit/delete-unit/delete-unit-controller-factory.ts
@@ -1,5 +1,5 @@
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-facotry'
-import { makeDbDeleteUnit } from '@/main/factories/usecases/unit/delete-unit/db-unit-company-factory'
+import { makeDbDeleteUnit } from '@/main/factories/usecases/unit/delete-unit/db-delete-unit-factory'
 import { DeleteUnitController } from '@/presentation/controllers/unit/delete-unit/delete-unit-controller'
 import { Controller } from '@/presentation/protocols'
 
