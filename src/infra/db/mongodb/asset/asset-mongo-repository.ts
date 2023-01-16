a5c4f5556450a6bab6b1919ee5419f68d662684d
sidneiarauj <sidnei.ifrn@gmail.com>
2023-01-15 23:02:06 -0300

    feat: create DeleteAsset Infra layer

--- src/infra/db/mongodb/asset/asset-mongo-repository.ts
+++ src/infra/db/mongodb/asset/asset-mongo-repository.ts
@@ -1,12 +1,14 @@
 import { AddAssetRepository } from '@/data/protocols/db/asset/add-asset-repository'
+import { DeleteAssetRepository } from '@/data/protocols/db/asset/delete-asset-repository'
 import { LoadAssetsByCompanyRepository } from '@/data/protocols/db/asset/load-assets-by-company-repository'
 import { LoadAssetsByUnitRepository } from '@/data/protocols/db/asset/load-assets-by-unit-repository'
 import { AssetModel } from '@/domain/models/asset'
 import { AddAssetModel } from '@/domain/usecases/asset/add-asset'
+import { ObjectId } from 'mongodb'
 import { MongoHelper } from '../helper/mongo-helper'
 
 export class AssetMongoRepository
-  implements AddAssetRepository, LoadAssetsByUnitRepository, LoadAssetsByCompanyRepository
+  implements AddAssetRepository, LoadAssetsByUnitRepository, LoadAssetsByCompanyRepository, DeleteAssetRepository
 {
   async add(assetData: AddAssetModel): Promise<AssetModel> {
     const assetCollection = await MongoHelper.getCollection('assets')
@@ -27,5 +29,15 @@ export class AssetMongoRepository
     const assetCollection = await MongoHelper.getCollection('assets')
     const assets: AssetModel[] = (await assetCollection.find({ companyId }).toArray()) as unknown as AssetModel[]
     return assets.map((asset) => MongoHelper.map(asset))
+  }
+
+  async delete(id: string): Promise<void> {
+    const mongoId = new ObjectId(id)
+    const assetCollection = await MongoHelper.getCollection('assets')
+    const mongoResponse = await assetCollection.deleteOne({ _id: mongoId })
+    const { deletedCount } = mongoResponse
+    if (deletedCount === 0) {
+      return null
+    }
   }
 }
